be85796a950c67ba3401d82d3289c9ec1e3ede10
ammesatyajit <ammesatyajit@gmail.com>
2025-03-07 22:48:13 -0800

    Revert "Merge branch 'satyajit/transformer-decoder' of https://github.com/ncylich/emg2qwerty into satyajit/transformer-decoder"
    This reverts commit 350044907b81bdcf4c5025590de443cddf6d2c3d, reversing
    changes made to d890272cbabd56337aa123cdc306ba4d43552807.

--- emg2qwerty/conformer_lightning.py
+++ emg2qwerty/conformer_lightning.py
@@ -5,11 +5,9 @@
 # LICENSE file in the root directory of this source tree.
 
 from collections.abc import Sequence
-from pathlib import Path
 from typing import Any, ClassVar, Optional
 
 import math
-import numpy as np
 import torch
 import pytorch_lightning as pl
 from hydra.utils import instantiate
@@ -30,114 +28,6 @@ from emg2qwerty.modules import (
     TDSConvEncoder,
 )
 from emg2qwerty.transforms import Transform
-
-
-class WindowedEMGDataModule(pl.LightningDataModule):
-    def __init__(
-        self,
-        window_length: int,
-        padding: tuple[int, int],
-        batch_size: int,
-        num_workers: int,
-        train_sessions: Sequence[Path],
-        val_sessions: Sequence[Path],
-        test_sessions: Sequence[Path],
-        train_transform: Transform[np.ndarray, torch.Tensor],
-        val_transform: Transform[np.ndarray, torch.Tensor],
-        test_transform: Transform[np.ndarray, torch.Tensor],
-    ) -> None:
-        super().__init__()
-
-        self.window_length = window_length
-        self.padding = padding
-
-        self.batch_size = batch_size
-        self.num_workers = num_workers
-
-        self.train_sessions = train_sessions
-        self.val_sessions = val_sessions
-        self.test_sessions = test_sessions
-
-        self.train_transform = train_transform
-        self.val_transform = val_transform
-        self.test_transform = test_transform
-
-    def setup(self, stage: str | None = None) -> None:
-        self.train_dataset = ConcatDataset(
-            [
-                WindowedEMGDataset(
-                    hdf5_path,
-                    transform=self.train_transform,
-                    window_length=self.window_length,
-                    padding=self.padding,
-                    jitter=True,
-                )
-                for hdf5_path in self.train_sessions
-            ]
-        )
-        self.val_dataset = ConcatDataset(
-            [
-                WindowedEMGDataset(
-                    hdf5_path,
-                    transform=self.val_transform,
-                    window_length=self.window_length,
-                    padding=self.padding,
-                    jitter=False,
-                )
-                for hdf5_path in self.val_sessions
-            ]
-        )
-        self.test_dataset = ConcatDataset(
-            [
-                WindowedEMGDataset(
-                    hdf5_path,
-                    transform=self.test_transform,
-                    # Feed the entire session at once without windowing/padding
-                    # at test time for more realism
-                    window_length=None,
-                    padding=(0, 0),
-                    jitter=False,
-                )
-                for hdf5_path in self.test_sessions
-            ]
-        )
-
-    def train_dataloader(self) -> DataLoader:
-        return DataLoader(
-            self.train_dataset,
-            batch_size=self.batch_size,
-            shuffle=True,
-            num_workers=self.num_workers,
-            collate_fn=WindowedEMGDataset.collate,
-            pin_memory=True,
-            persistent_workers=True,
-        )
-
-    def val_dataloader(self) -> DataLoader:
-        return DataLoader(
-            self.val_dataset,
-            batch_size=self.batch_size,
-            shuffle=False,
-            num_workers=self.num_workers,
-            collate_fn=WindowedEMGDataset.collate,
-            pin_memory=True,
-            persistent_workers=True,
-        )
-
-    def test_dataloader(self) -> DataLoader:
-        # Test dataset does not involve windowing and entire sessions are
-        # fed at once. Limit batch size to 1 to fit within GPU memory and
-        # avoid any influence of padding (while collating multiple batch items)
-        # in test scores.
-        return DataLoader(
-            self.test_dataset,
-            batch_size=1,
-            shuffle=False,
-            num_workers=self.num_workers,
-            collate_fn=WindowedEMGDataset.collate,
-            pin_memory=True,
-            persistent_workers=True,
-        )
 
 
 # Positional Encoding for transformer inputs
