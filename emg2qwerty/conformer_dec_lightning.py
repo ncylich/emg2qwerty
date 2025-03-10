2783d84220aa2d8817692c687f934b7824542476
Noah Cylich <noahcylich@gmail.com>
2025-03-10 14:46:42 -0700

    fixed testing datasetloader so testing actually works

--- emg2qwerty/conformer_dec_lightning.py
+++ emg2qwerty/conformer_dec_lightning.py
@@ -95,10 +95,8 @@ class WindowedEMGDataModule(pl.LightningDataModule):
                 WindowedEMGDataset(
                     hdf5_path,
                     transform=self.test_transform,
-                    # Feed the entire session at once without windowing/padding
-                    # at test time for more realism
-                    window_length=None,
-                    padding=(0, 0),
+                    window_length=self.window_length,
+                    padding=self.padding,
                     jitter=False,
                 )
                 for hdf5_path in self.test_sessions
