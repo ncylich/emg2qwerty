fd4fd1a416ac6f7c573c2c5861748cd82b740f07
Noah Cylich <noahcylich@gmail.com>
2025-03-12 15:21:06 -0700

    made venkat's naming more consistent

---
+++ run_tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# val/CER=36.59
+#python -m emg2qwerty.train --config-name=log_bin_conformer_decoder
+#echo "log_bin_conformer_decoder done" && echo " "
+
+# val/CER=34.95
+#python -m emg2qwerty.train --config-name=dct_conformer_decoder
+#echo "dct_conformer_decoder done" && echo " "
+
+# val/CER=31.35
+#python -m emg2qwerty.train --config-name=conformer_decoder
+#echo "conformer_decoder done" && echo " "
+
+# val/CER=62.44
+#python -m emg2qwerty.train --config-name=log_bin_base
+#echo "log_bin_base done" && echo ""
+
+# val/CER=40.89
+#python -m emg2qwerty.train --config-name=dct_base
+#echo "dct_base done" && echo ""
+
+
+# val/CER=36.66
+#python -m emg2qwerty.train --config-name=base
+#echo "50 base done" && echo " "
+
+# val/CER=40.54
+#python -m emg2qwerty.train --config-name=150_base
+#echo "150 base done" && echo " "
+#echo "All tests done"
